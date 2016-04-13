fd89209694183de196f9867b27933779f48399aa
Stefan Schlenker <stefan.schlenker@cern.ch>
2016-04-13 15:11:11 +0200

    module example

---
+++ MyQuasarModule/include/MyQuasarModuleClass.h
@@ -0,0 +1,18 @@
+/* © Copyright CERN, 2015. All rights not expressly granted are reserved. */
+
+#ifndef MYQUASARMODULECLASS_H_
+#define MYQUASARMODULECLASS_H_
+
+namespace MyQuasarModule
+{
+
+ class MyQuasarModuleClass
+ {
+ public:
+	 MyQuasarModuleClass();
+	 ~MyQuasarModuleClass();
+ };
+
+} // namespace
+
+#endif /* MYQUASARMODULECLASS_H_ */
---
+++ MyQuasarModule/src/MyQuasarModuleClass.cpp
@@ -0,0 +1,17 @@
+#include <MyQuasarModuleClass.h>
+
+#include <iostream>
+
+using namespace MyQuasarModule;
+
+MyQuasarModuleClass::MyQuasarModuleClass()
+{
+	// FIXME: add component logging
+	std::cout << "ctor MyQuasarModuleClass." << std::endl;
+}
+
+MyQuasarModuleClass::~MyQuasarModuleClass()
+{
+	// FIXME: add component logging
+	std::cout << "dtor MyQuasarModuleClass." << std::endl;
+}
